ecfa21a4ed67917a0d065b26f1e14b92a5faf146
Shadeling <mironow.slav@yandex.ru>
2017-12-06 12:38:01 +0300

    Add files via upload

---
+++ lab4-5, DZ_csh/lab4.csh/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using ab5library;
+using System.Threading.Tasks;
+
+namespace lab4.csh
+{ 
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "текстовые файлы|*.txt";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                string text = File.ReadAllText(fd.FileName);
+                char[] separators = new char[] { ' ', '.', ',', '!', '?', '/', '\t', '\n' };
+                string[] textArray = text.Split(separators);
+                foreach (string strTemp in textArray)
+                {
+                    string str = strTemp.Trim();
+                    if (!listBoxResult.Items.Contains(str)) listBoxResult.Items.Add(str);
+                }
+                t.Stop();
+                this.textBoxFileReadTime.Text = t.Elapsed.ToString();
+                this.textBoxFileReadCount.Text = listBoxResult.Items.Count.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл");
+            }
+        }
+
+        private void buttonExact_Click(object sender, EventArgs e)
+        {
+            string word = this.textBoxFind.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(word) && listBoxResult.Items.Count > 0)
+            {
+                string wordUpper = word.ToUpper();
+                List<string> tempList = new List<string>();
+                Stopwatch t = new Stopwatch();
+                t.Start();
+                foreach (string str in listBoxResult.Items)
+                {
+                    if (str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                }
+                t.Stop();
+                this.textBoxExactTime.Text = t.Elapsed.ToString();
+                this.listBoxResult.BeginUpdate();
+                this.listBoxResult.Items.Clear();
+                foreach (string str in tempList)
+                {
+                    this.listBoxResult.Items.Add(str);
+                }
+                this.listBoxResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+        }
+
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonSaveReport_Click(object sender, EventArgs e)
+        {
+            string TempReportFileName = "Report_" + DateTime.Now.ToString("dd_MM_yyyy_hhmmss");
+            //Диалог сохранения файла отчета
+            SaveFileDialog fd = new SaveFileDialog();
+            fd.FileName = TempReportFileName;
+            fd.DefaultExt = ".html";
+            fd.Filter = "HTML Reports|*.html";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                string ReportFileName = fd.FileName;
+                //Формирование отчета
+                StringBuilder b = new StringBuilder();
+                b.AppendLine("<html>");
+                b.AppendLine("<head>");
+                b.AppendLine("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>");
+                b.AppendLine("<title>" + "Отчет: " + ReportFileName + "</title>");
+                b.AppendLine("</head>");
+                b.AppendLine("<body>");
+                b.AppendLine("<h1>" + "Отчет: " + ReportFileName + "</h1>");
+                b.AppendLine("<table border='1'>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Время чтения из файла</td>");
+                b.AppendLine("<td>" + this.textBoxFileReadTime.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Количество уникальных слов в файле</td>");
+                b.AppendLine("<td>" + this.textBoxFileReadCount.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Слово для поиска</td>");
+                b.AppendLine("<td>" + this.textBoxFind.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Максимальное расстояние для нечеткого поиска</td>");
+                b.AppendLine("<td>" + this.textBoxMaxDist.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Время четкого поиска</td>");
+                b.AppendLine("<td>" + this.textBoxExactTime.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr>");
+                b.AppendLine("<td>Время нечеткого поиска</td>");
+                b.AppendLine("<td>" + this.textBoxApproxTime.Text + "</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("<tr valign='top'>");
+                b.AppendLine("<td>Результаты поиска</td>");
+                b.AppendLine("<td>");
+                b.AppendLine("<ul>");
+                foreach (var x in this.listBoxResult.Items)
+                {
+                    b.AppendLine("<li>" + x.ToString() + "</li>");
+                }
+                b.AppendLine("</ul>");
+                b.AppendLine("</td>");
+                b.AppendLine("</tr>");
+                b.AppendLine("</table>");
+                b.AppendLine("</body>");
+                b.AppendLine("</html>");
+                //Сохранение файла
+                File.AppendAllText(ReportFileName, b.ToString());
+                MessageBox.Show("Отчет сформирован. Файл: " + ReportFileName);
+            }
+
+        }
+
+        private void buttonApprox_Click(object sender, EventArgs e)
+        {
+            string word = this.textBoxFind.Text.Trim();
+            if (!string.IsNullOrWhiteSpace(word) && listBoxResult.Items.Count > 0)
+            {
+                int maxDist;
+                if (!int.TryParse(this.textBoxMaxDist.Text.Trim(), out maxDist))
+                {
+                    MessageBox.Show("Необходимо указать максимальное расстояние");
+                    return;
+                }
+                if (maxDist < 1 || maxDist > 5)
+                {
+                    MessageBox.Show("Максимальное расстояние должно быть в диапазоне от 1 до 5");
+                    return;
+                }
+                int ThreadCount;
+                if (!int.TryParse(this.textBoxThreads.Text.Trim(), out ThreadCount))
+                {
+                    MessageBox.Show("Необходимо указать количество потоков"); return;
+                }
+
+                string wordUpper = word.ToUpper();
+                List<Tuple<string, int>> tempList = new List<Tuple<string, int>>();
+
+                Stopwatch t = new Stopwatch();
+                t.Start();
+
+                List<ParallelSearchRes> Result = new List<ParallelSearchRes>();
+                List<MinMax> arrayDivList = SubArrays.DivideSubArrays(0, listBoxResult.Items.Count, ThreadCount);
+                int count = arrayDivList.Count;
+                Task<List<ParallelSearchRes>>[] tasks = new Task<List<ParallelSearchRes>>[count];
+
+                for (int i = 0; i < count; i++)
+                {
+                    List<string> tempTaskList = new List<string>();
+                    for (int i1=arrayDivList[i].Min;i1<=arrayDivList[i].Max - arrayDivList[i].Min; i1++ )
+                    {
+                        tempTaskList.Add(listBoxResult.Items[i1].ToString());
+                    }
+                    tasks[i] = new Task<List<ParallelSearchRes>>( Program.ArrayThreadTask, new ParallelSearchThreadParam() { tempList = tempTaskList, maxDist = maxDist, ThreadNum = i, wordPattern = word });
+                    tasks[i].Start();
+                }
+                Task.WaitAll(tasks);
+
+                /*foreach (string str in listBoxResult.Items)
+                {
+                    //Вычисление расстояния Дамерау-Левенштейна
+                    int dist = ab5library.EditDistanse.Distance(str.ToUpper(), wordUpper);
+                    //Если расстояние меньше порогового, то слово добавляется в результат
+                    if (dist <= maxDist)
+                    {
+                        tempList.Add(new Tuple<string, int>(str, dist));
+                    }
+                }*/
+
+                t.Stop();
+                for (int i = 0; i < count; i++) { Result.AddRange(tasks[i].Result); }
+
+                this.textBoxApproxTime.Text = t.Elapsed.ToString();
+                this.listBoxResult.BeginUpdate();
+                this.listBoxResult.Items.Clear();
+                foreach (var x in Result)
+                {
+                    string temp = x.word + "(расстояние=" + x.dist.ToString() + " поток=" + x.ThreadNum.ToString() + ")";
+                    this.listBoxResult.Items.Add(temp);
+                }
+                /*foreach (var x in tempList)
+                {
+                    string temp = x.Item1 + "(расстояние=" + x.Item2.ToString() + ")";
+                    this.listBoxResult.Items.Add(temp);
+                }*/
+                this.listBoxResult.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл и ввести слово для поиска");
+            }
+        }
+       
+    }
+}
---
+++ lab4-5, DZ_csh/lab4.csh/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace lab4.csh
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static List<ParallelSearchRes> ArrayThreadTask(object paramObj)
+        {
+            ParallelSearchThreadParam param = (ParallelSearchThreadParam)paramObj;
+            string wordUpper = param.wordPattern.Trim().ToUpper();
+            List<ParallelSearchRes> Result = new List<ParallelSearchRes>();
+            foreach (string str in param.tempList)
+            {
+                int dist = ab5library.EditDistanse.Distance(str.ToUpper(), wordUpper);
+                if (dist <= param.maxDist)
+                {
+                    ParallelSearchRes temp = new ParallelSearchRes() { word = str, dist = dist, ThreadNum = param.ThreadNum };
+                    Result.Add(temp);
+                }
+            }
+            return Result;
+        }
+    }
+}
